71e8161d5440b1fc934c1ac8f5e79fd1cb258f8a
balibalbal <balibalbal@gmail.com>
2025-12-11 23:52:03 +0700

    update monitoring

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -108,9 +108,9 @@
                   <div class="card">
                     <div class="card-header d-flex align-items-center justify-content-between">
                       <div class="card-title mb-0">
-                        <h5><i class="mdi mdi-excavator mdi-36px" style="color: red;"></i> Excavator Kemiringan > 3° ({{ $offlineCount }} unit)</h5><a href="{{ route('reports.index') }}" class="btn rounded-pill btn-success waves-effect waves-light"><i class="mdi mdi-file-document-outline"></i> Selengkapnya</a>
+                        <h5><i class="mdi mdi-excavator mdi-36px" style="color: red;"></i> Excavator Kemiringan > 3° ({{ $offlineCount }} unit)</h5>
                       </div>
-                      
+                      <a href="{{ route('reports.index') }}" class="btn rounded-pill btn-success waves-effect waves-light"><i class="mdi mdi-file-document-outline"></i> Selengkapnya</a>
                     </div>
                     <div class="card-datatable table-responsive">
                       <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
